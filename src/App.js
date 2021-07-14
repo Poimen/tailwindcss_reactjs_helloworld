994e41d74aed6f064c576be8d03875a4b181141c
Richard Shephard <richard.shephard@gmail.com>
2021-07-14 14:32:33 +0200

    update deps and enable tailwind jit syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import TW from './components/TailwindJIT';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col gap-8 App">
       <HelloWorld msg="Hello from Tailwind inside ReactJS"></HelloWorld>
+      <TW />
     </div>
   );
 }
